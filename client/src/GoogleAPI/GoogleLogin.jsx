2291efdac37c45915332cd26519573126deac958
joseph <joewxlk3r@gmail.com>
2022-07-11 14:24:43 +0800

    added google api file to handle google login requests

---
+++ client/src/GoogleAPI/GoogleLogin.jsx
@@ -0,0 +1,13 @@
+import './googleApi.css'
+
+const GoogleLoginButton = () => {
+    return (
+        <>
+            <button className="google-login-button">
+                Login with Google
+            </button>
+        </>
+     );
+}
+
+export default GoogleLoginButton;
