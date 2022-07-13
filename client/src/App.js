595a20d2539e2cb3455f4873067df098fa31c262
joseph <joewxlk3r@gmail.com>
2022-07-13 20:04:44 +0800

    adding stripe features, ui fixes

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,48 @@ import Header from './Components/HeaderComponents/Header';
 import EventHeader from './Components/Eventheadercomponents/EventHeader';
 import Footer from './Components/FooterComponents/Footer';
 
-function App({bool, onToggle}) {
-
+function App({bool, onToggle, quantity, amount, currency, onSubmit, type, action, method, click, handleQuantities, id, addItems, price, items, cartItems, updateCartItems, removeCartItem, mobile }) {
   return (
     <div className={'app-container-' + bool['sidebar']} >
       <Header bool={bool}
-        onToggle={onToggle}/>
-      <EventHeader/>
-      <Main />
-      <ShopItems />
-      <Footer/>
+        onToggle={onToggle}
+        quantity={quantity}
+        formatPrice={amount}
+        amount={amount}
+        currency={currency}
+        type={type}
+        click={click}
+        handleQuantities={handleQuantities}
+        onSubmit={onSubmit}
+        action={action}
+        method={method}
+        items={items}
+        cartItems={cartItems}
+        id={id}
+        removeCartItem={removeCartItem}
+        mobile={mobile}
+      />
+      <EventHeader />
+      <Main mobile={mobile} bool={bool} />
+      <ShopItems
+        quantity={quantity}
+        formatPrice={amount}
+        amount={amount}
+        currency={currency}
+        type={type}
+        click={click}
+        handleQuantities={handleQuantities}
+        onSubmit={onSubmit}
+        action={action}
+        method={method}
+        addItems={addItems}
+        id={id}
+        cartItems={cartItems}
+        updateCartItems={updateCartItems}
+        mobile={mobile} />
+      
+      
+      <Footer />
     </div>
   );
 }
