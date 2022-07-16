9829ea843a44f085b2f876dca4c5aeda5b010e52
joseph <joewxlk3r@gmail.com>
2022-07-16 19:12:55 +0800

    add Item.jsx

--- client/src/Stripe/Item.jsx
+++ client/src/Stripe/Item.jsx
@@ -1,31 +1,59 @@
+
+import { useEffect } from "react";
 import { useCallback, useState } from "react";
+import { setLocalData } from "../Services/handleLocalData";
+import './items.css'
 import './ShopItemsComponents/ShopItems.css'
 
-const Item = ({ quantity, amount, currency, onSubmit, alt, src, type, submit, action, method, click, quantityClick, updateCartItems, id}) => {
+const Item = ({ items, data, updateCartItems }) => {
     
-    const handleClick = useCallback((props) => {
-        quantityClick(quantity,props)
-    })
-    
+    const handleCartUpdate = useCallback((item) => {
+        updateCartItems(item);
+      })
 
     return (
         <div className="sr-main">
-            <section className="container">
-                <div>
-                    <h1>{id}</h1>
-                    <h4>{currency}</h4>
-                    <div className="pasha-image">
-                        <img
-                            alt={alt}
-                            src={src}
-                        />
+            {items.map(({id, currency, images, description, active, unit_amount, name, product, custom_unit_amount }) => { 
+                if (!active) {
+                    return (
+                        <section key={id} className="main-shopitem-container">
+                        <div className='main-shopitem-text-container'>
+                            <div className="image">
+                                <h1 > {id} </h1>
+                                <img alt={images} src={images} className='product-image' />
+                            </div>
+                            <div  className='description-container'>
+                                <p>{description}</p>
+                            </div>
+                        </div>
+                        <form className='main-logic-container'>
+                            <p className="sr-legal-text" >{unit_amount}</p>
+                                <button onClick={e => { e.preventDefault(); handleCartUpdate({ id: product, amount: '', unit_amount: unit_amount, images: images}) }}>add to cart</button>
+                        </form>
+                    </section>
+                    )
+                }
+                return (
+                    <>
+                    <section className="main-shopitem-container">
+                    <div className='main-shopitem-text-container'>
+                        <div className="image">
+                            <h1>{id}</h1>
+                            <img alt={images} src={images} className='product-image' />
+                        </div>
+                        <div className='description-container'>
+                            <p>{description}</p>
+                        </div>
                     </div>
-                </div>
-                <form
-                >
-                    <p className="sr-legal-text">{`$${amount}`}</p>
-                </form>
-            </section>
+                    <form className='main-logic-container'>
+                        <p className="sr-legal-text">{`$${unit_amount}`}</p>
+                        <button onClick={e => { e.preventDefault(); handleCartUpdate({ id: product, amount: '', unit_amount: unit_amount, images: images}) }}>add to cart</button>
+                    </form>
+                    </section>
+                    </>
+                )
+            })}
+           
         </div>
     );
 }
