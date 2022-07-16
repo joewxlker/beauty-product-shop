71595104afd8cfc0c546508e67b86ad87a6aa6f7
joseph <joewxlk3r@gmail.com>
2022-07-16 19:10:44 +0800

    updated props to match cart feature updates

--- client/src/Stripe/ShopItemsComponents/ShopItems.jsx
+++ client/src/Stripe/ShopItemsComponents/ShopItems.jsx
@@ -2,30 +2,16 @@ import './ShopItems.css'
 import Item from '../Item';
 import { useCallback } from 'react';
 
-const ShopItems = ({ quantity, amount, currency, click, quantityClick, id, cartItems, updateCartItems}) => {
-
-    const handleCartUpdate = useCallback(() => {
-        for (let v in cartItems) {
-            if(cartItems[v] === id) return
-        }
-        updateCartItems(id)
-    })
-
+const ShopItems = ({ cartItems, updateCartItems, items, data }) => {
+    
     return (
         <>
             <h1>SHOP ALL ITEMS</h1>
             <Item
-                cartItems={cartItems}
+                items={items}
+                data={data}
                 updateCartItems={updateCartItems}
-                quantity={quantity}
-                formatPrice={amount}
-                amount={amount}
-                currency={currency}
-                type={'main'}
-                click={click}
-                quantityClick={quantityClick} 
-                id={id} />
-            <button onClick={handleCartUpdate}>add to cart</button>
+                cartItems={cartItems} />
         </>
     );
 }
