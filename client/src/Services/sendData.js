f5184c03429ea1697950f985cfe4c81b6be25db3
joseph <joewxlk3r@gmail.com>
2022-07-16 19:02:15 +0800

    formatting

--- client/src/Services/sendData.js
+++ client/src/Services/sendData.js
@@ -1,12 +1,21 @@
-const sendData = async (target, value) => {
+export const sendData = async (target, value) => {
+    let output = '';
+    console.log(value)
+    await fetch(`http://localhost:5000/api/${target}`, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(value)
+    }).then((res) => res.json())
+        .then((data) => { output = data })
+    return output
+}
+
+export const getData = async (target) => {
     let output = '';
     await fetch(`http://localhost:5000/api/${target}`,{
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(value)
+        method: 'get',
+        data: 'json',
     }).then((res) => res.json())
         .then((data) => { output = data})
     return output
 }
-
-export default sendData
