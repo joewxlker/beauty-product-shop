b1b8cab7e596fb9e48135046f4f90ff5a7319096
joseph <joewxlk3r@gmail.com>
2022-07-16 19:04:53 +0800

    updated props to include new cart Obj

--- client/src/Stripe/CartComponents/Cart.jsx
+++ client/src/Stripe/CartComponents/Cart.jsx
@@ -1,31 +1,47 @@
+import { useEffect, useState } from 'react';
 import { useCallback } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import { getLocalData, setLocalData } from '../../Services/handleLocalData';
 import CartItems from './CartItems';
 
-const Cart = ({ quantity, formatPrice, amount, currency, handleQuantities, cartItems, removeCartItem, id }) => {
+const Cart = ({ cartItems, quantity, onRemoveItem, data, handleQuantityChange }) => {
+
+    // const [total, setTotal] = useState();
+
+    // useEffect(() => {
+    //     console.log(cartItems)
+    //     let sum = 0;
+    //     for (let v in cartItems) {
+    //         sum = sum + cartItems[v].unit_amount;
+    //     }
+    //     setTotal(sum)
+    // }, [setTotal])
     
     return (
         <>
-            {cartItems.map((cart) => {
+            {cartItems.map(({id, unit_amount, images, amount }) => {
                 return (
                     <>
                         <div className='cart-container'>
                             <CartItems
-                                quantity={quantity}
-                                formatPrice={formatPrice}
-                                amount={amount}
-                                currency={currency}
-                                handleQuantities={handleQuantities}
-                                removeCartItem={removeCartItem}
+                                quantity={amount}
+                                handleQuantityChange={handleQuantityChange}
+                                onRemoveItem={onRemoveItem}
                                 cartItems={cartItems}
+                                data={data}
                                 id={id}
+                                amount={unit_amount}
+                                images={images}
                             />
                         </div>
                     </>
                 )
             })}
-            <p>Total: </p>
-            <Link to='/Checkout'><button>checkout</button></Link>
+            {/* <p>Total: {total}</p> */}
+            {cartItems.length !== undefined &&
+                <Link to='/checkout'>
+                    <button >checkout</button>
+                </Link>}
                         
         </>
 
