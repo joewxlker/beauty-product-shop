735b67867c84d9b948312b15209b4a8481703c07
joseph <joewxlk3r@gmail.com>
2022-07-16 19:18:04 +0800

    fixing props issues, removed props from checkout

--- client/src/index.js
+++ client/src/index.js
@@ -12,12 +12,9 @@ import Success from './Stripe/Checkout/Success';
 
 const Index = () => {
   
-  const [currency, setCurrency] = useState('AUD');
-  const [quantity, setQuantity] = useState(1);
-  const [cartItems, setCart] = useState([]);
-  const [price, setPrice] = useState([]);
   const [bool, setBool] = useSetBool();
   const [mobile, setIsMobile] = useState(false)
+
 
   useEffect(() => {
       if (window.innerWidth < 600) setIsMobile(true)
@@ -27,88 +24,33 @@ const Index = () => {
           if (window.innerWidth < 600) setIsMobile(true)
           if (window.innerWidth > 600) setIsMobile(false)
       });
-    console.log(mobile)
+    
   }, [mobile]);
 
-  useEffect(() => {
-    async function fetchConfig() {
-      await fetch('/config')
-        .then(r => r.json())
-        .then((data) => {
-            setPrice(data)
-        })
-    }
-    fetchConfig();
-    console.log(price)
-  }, []);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  }
-
-  const handleClick = (x) => {
-    console.log(x)
-  }
-  
-  const addItem = (newPrice, newQuantity) => {
-    console.log(price)
-  }
-
-  const handleQuantity = (e) => {
-    if (e) setQuantity(quantity + 1);
-    else setQuantity(quantity - 1);
-
-  }
   return (
-    <BrowserRouter>
-      <Routes>
-        {price.map((price) => {
-          return (
-          <>
-            <Route path="/" element={
-                <App
-                mobile={mobile}
-                bool={bool}
-                onToggle={setBool}
-                quantity={quantity}
-                formatPrice={price.price.unit_amount}
-                amount={price.price.unit_amount}
-                currency={price.price.currency}
-                type={'main'}
-                click={handleClick}
-                handleQuantities={handleQuantity}
-                onSubmit={handleSubmit}
-                id={price.publicKey}
-                cartItems={cartItems}
-                updateCartItems={(item) => { setCart(prev => [...prev, item]); }}
-                removeCartItem={(item) => { setCart( cartItems[item] === undefined)}}
-              />
-              
-            } />
-            
-            <Route path="/checkout"
-              element={
-                <Checkout/>
-              }
-            />
-          </>)})}
-        <Route path="/createaccount" element={
-          <CreateAccount
-            bool={bool}
-            onToggle={setBool}
-            cartItems={cartItems}
-            removeCartItem={(item) => { setCart( cartItems[item] === undefined)}}  />
-        } />
-        <Route path="*" element={
-          <></>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <App
+              mobile={mobile}
+              bool={bool}
+              onToggle={setBool} />
           } />
-        <Route path="/success" element={<Success />} />
-        <Route path="/canceled" element={<Canceled />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/createaccount" element={
+            <CreateAccount
+              bool={bool}
+              onToggle={setBool}/>
+          } />
+          <Route path="*" element={<></>} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/canceled" element={<Canceled />} />
 
-      </Routes>
-    </BrowserRouter>
-
+        </Routes>
+      </BrowserRouter>
+    </>
   )
-  // <App/>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
