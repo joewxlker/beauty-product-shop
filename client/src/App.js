f0b2c4d612b2f9742faaa648ce7afec4af24da7d
joseph <joewxlk3r@gmail.com>
2022-07-16 19:16:40 +0800

    updated cart state properties and handling, updated localStorageData properties and handling

--- client/src/App.js
+++ client/src/App.js
@@ -4,48 +4,99 @@ import ShopItems from './Stripe/ShopItemsComponents/ShopItems.jsx'
 import Header from './Components/HeaderComponents/Header';
 import EventHeader from './Components/Eventheadercomponents/EventHeader';
 import Footer from './Components/FooterComponents/Footer';
+import { useEffect, useState } from 'react';
+import { getData } from './Services/sendData';
+import { getLocalData, setLocalData } from './Services/handleLocalData';
 
-function App({bool, onToggle, quantity, amount, currency, onSubmit, type, action, method, click, handleQuantities, id, addItems, price, items, cartItems, updateCartItems, removeCartItem, mobile }) {
+function App({ bool, onToggle, mobile }) {
+
+  const [price, setPrice] = useState([]);
+  const [data, setData] = useState([])
+  const [cartItems, setCart] = useState([]);
+
+  useEffect(() => {
+    getLocalData('cart').then((data) => {
+      if (data === null) {
+        setData([]);
+        setCart([]);
+        return
+      }
+      console.log('setting data')
+      setData(data);
+      setCart(data);
+    })
+    async function fetchConfig() {
+      await getData('config').then((data) => {
+        for (let v in data.items.data) {
+          setPrice(prev => [...prev, { ...data.items.data[v], ...data.price.data[v] }]);
+        }     
+      })
+    }
+    fetchConfig()
+  }, [])
+
+  useEffect(() => {
+    if(cartItems.length === 0) return
+    console.log(cartItems, localStorage)
+    setLocalData('cart', cartItems)
+    setData(getLocalData('cart'))
+  }, [cartItems, setCart])
+
+  const removeItem = (item) => {
+    for (let v in cartItems) {
+      if (cartItems[v].id === item) {
+        if (cartItems.length === 1) {
+          console.log('shift')
+          setCart([]);
+          setLocalData('cart', [])
+        }
+        else {
+          console.log('splice')
+          setCart([...cartItems.splice(cartItems.indexOf(cartItems[v]), 1)])
+          setLocalData('cart', cartItems)
+        }
+      }
+    }
+  }
+  const addItem = (item) => {
+    for (let v in cartItems) { if (cartItems[v].id === item.id) return }
+    if (Object.keys(cartItems).length > 1) { console.log('keys = obj'); return setCart(item)}
+    setCart(prev => [...prev, item])
+  }
+  const handleQuantity = (count, id) => {
+    console.log(count, id)
+    let allItems = [];
+    let updatedValue;
+    for (let v in cartItems) {
+      if (cartItems[v].id !== id) {
+      allItems.push(cartItems[v])}
+      if (cartItems[v].id === id) {
+        updatedValue = [{...cartItems[v], ...{ amount: count }}];
+      }
+      const newCart = [...allItems, ...updatedValue];
+      setLocalData('cart', newCart)
+    }
+    
+    
+  }
+
   return (
     <div className={'app-container-' + bool['sidebar']} >
-      <Header bool={bool}
-        onToggle={onToggle}
-        quantity={quantity}
-        formatPrice={amount}
-        amount={amount}
-        currency={currency}
-        type={type}
-        click={click}
-        handleQuantities={handleQuantities}
-        onSubmit={onSubmit}
-        action={action}
-        method={method}
-        items={items}
-        cartItems={cartItems}
-        id={id}
-        removeCartItem={removeCartItem}
+      <Header
+        bool={bool}
         mobile={mobile}
+        onToggle={onToggle}
+        data={data}
+        onRemoveItem={removeItem}
+        handleQuantityChange={handleQuantity}
+        cartItems={cartItems}
       />
       <EventHeader />
       <Main mobile={mobile} bool={bool} />
       <ShopItems
-        quantity={quantity}
-        formatPrice={amount}
-        amount={amount}
-        currency={currency}
-        type={type}
-        click={click}
-        handleQuantities={handleQuantities}
-        onSubmit={onSubmit}
-        action={action}
-        method={method}
-        addItems={addItems}
-        id={id}
-        cartItems={cartItems}
-        updateCartItems={updateCartItems}
-        mobile={mobile} />
-      
-      
+        items={price}
+        data={data}
+        updateCartItems={addItem}/>
       <Footer />
     </div>
   );
