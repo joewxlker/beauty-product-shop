addcb1b1a7ee91bc5ebf80ac30a3d234289e812a
joseph <joewxlk3r@gmail.com>
2022-07-20 21:12:09 +0800

    added ProductPages to directory

--- client/src/Components/ProductPages/ProductsMain.jsx
+++ client/src/Components/ProductPages/ProductsMain.jsx
@@ -1,7 +1,28 @@
-const ProductsOne = () => {
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+import ShopItems from "../../Stripe/ShopItemsComponents/ShopItems";
+import Header from "../HeaderComponents/Header";
+import Footer from '../FooterComponents/Footer'
+import '../HeaderComponents/HeaderBig.css'
+
+const ProductsOne = ({ updateCartItems, items, cartItems, data }) => {
+    
+    const location = useLocation();
+    console.log(cartItems)
+    useEffect(() => { 
+        setTimeout(() => {
+            console.log(location)            
+        }, 1000);
+    }, [])
+
+        
     return (
-        <div className='products-container'>
-        </div>
+        <>
+            <ShopItems
+                items={items}
+                data={data}
+                updateCartItems={updateCartItems} />
+        </>
     );
 }
 
