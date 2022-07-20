f7831adf9f0d562df952633a47fc7c49c4f4e84a
joseph <joewxlk3r@gmail.com>
2022-07-20 21:15:10 +0800

    debugging

--- client/src/Hooks/setBoolean.jsx
+++ client/src/Hooks/setBoolean.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const useSetBool = () => {
     const [bool, setBool] = useState({'sidebar': false});
 
-    return [bool, (type,props)=> { setBool({ ...bool, [type]: props })}]
+    return [bool, (type, props) => { setBool({ ...bool, [type]: props }); console.log(type, props)}]
 }
 
 export default useSetBool
