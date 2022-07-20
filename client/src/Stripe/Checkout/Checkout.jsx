fc10fdaab0193cc6a46183e7d87d7df4b76ea0de
joseph <joewxlk3r@gmail.com>
2022-07-20 21:16:55 +0800

    prop management to improve reusability within other components

--- client/src/Stripe/Checkout/Checkout.jsx
+++ client/src/Stripe/Checkout/Checkout.jsx
@@ -1,18 +1,28 @@
 import './checkout.css'
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { getLocalData } from '../../Services/handleLocalData';
-import { formatPrice } from '../../Services/formatPrice';
-import { getData, sendData } from '../../Services/sendData';
+import { sendData } from '../../Services/sendData';
+import CreateForm from '../../UserAuth/Signup/CreateForm';
 
-const Checkout = () => {
+const Checkout = ({onRemoveItem, handleQuantityChange}) => {
   
   const [data, setData] = useState()
   const [dataSet, setDataSet] = useState();
   const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState();
+
+  const types = ['street', 'city', 'state', 'postal code'] 
+  const props = [];
 
   useEffect(() => {
-    const init = async () => {
+    setLoading(false)
+  }, [])
+  useEffect(() => {
+    console.log('jackson')
+    init();
+  }, [onRemoveItem, handleQuantityChange])
+    
+  const init = async () => {
     getLocalData('cart').then(async(callData) => {
       setData(callData);
       await sendData('config', callData).then((data) => setDataSet(data));
@@ -21,44 +31,54 @@ const Checkout = () => {
         const product = callData[v].unit_amount * callData[v].amount
         sum += product;
       }
-      setTotal(sum);
+      setTotal(sum/100);
     })
     }
-    init();
-  }, [])
-    
   const sendBuyRequest = (e) => {
     e.preventDefault();
+    setLoading(true);
     let arr = []
     for (let v in data) {
-        const { id } = dataSet[v]
-        const {amount} = data[v]
+      const { id } = dataSet[v]
+      const { amount } = data[v]
       arr.push({ price: id, quantity: amount })
       console.log(arr)
 
     }
-    sendData('create-checkout-session', arr).then((data) => window.location.href = data.url)
+    sendData('create-checkout-session', arr)
+      .then((data) => {
+        window.location.href = data.url
+      })
   }
   
   if(data === undefined) return
   return (
-    <>
-      
-      <Link to='/'>Home</Link>
-      {data.map(({ images, id, unit_amount, amount, name }) => {
-        return (
+    <div className='checkout-main-container'>
+      <div className='checkout-items-container'>
+        {data.map(({ images, id, unit_amount, amount, name }) => {
+          return (
             <div className="sr-root">
               <img src={images} height={'100px'} width={'100px'} />
               <h1>{name}</h1>
               <p><strong>Quantity {amount}</strong></p>
-            <p>{unit_amount} x {amount} = {unit_amount * amount}</p>
+              <p>${unit_amount/100} x {amount} = ${(unit_amount/100) * amount}</p>
 
             </div>
-        )
-      })}
+          )
+        })}
         <p>Total: ${total}</p>
-      <button onClick={sendBuyRequest}>Proceed to payment</button>
-    </>
+      </div>
+      <div className='checkout-side-container'>
+        <CreateForm
+          types={types}
+          props={props}
+          origin={'checkout'}
+        />
+        {/**TODO, fix infinite load when proceed clicked before data can load */}
+        {!loading && <button className='payment-button' onClick={sendBuyRequest}><span className='spinner-span'><p>Proceed to payment</p></span></button>}
+        {loading && <button className='payment-button' disabled><span className='spinner-span'><p>Proceed to payment</p><i class="fak fa-circle-notch-thin"></i></span></button>}
+      </div>
+    </div>
   )
 };
 export default Checkout;
--- client/src/Stripe/Item.jsx
+++ client/src/Stripe/Item.jsx
@@ -7,55 +7,70 @@ import './ShopItemsComponents/ShopItems.css'
 
 const Item = ({ items, data, updateCartItems }) => {
     
+    useEffect(() => {
+        if (items === undefined) {
+            console.log(loading, items)
+           return setLoading(true)
+        }
+        setLoading(false)
+        console.log(loading, items)
+    }, [items])
+    const [loading, setLoading] = useState(true);
     const handleCartUpdate = useCallback((item) => {
         updateCartItems(item);
         console.log(items)
       })
 
     return (
-        <div className="sr-main">
-            {items.map(({id, images, description, active, unit_amount, product, name, currency }) => { 
-                if (!active) {
-                    return (
-                        <section key={id} className="main-shopitem-container">
-                        <div className='main-shopitem-text-container'>
-                            <div className="image">
-                                <h1 > {name} </h1>
-                                <img alt={images} src={images} className='product-image' />
-                            </div>
-                            <div  className='description-container'>
-                                <p>{description}</p>
-                            </div>
-                        </div>
-                        <form className='main-logic-container'>
-                            <p className="sr-legal-text" >{unit_amount}</p>
-                                <button onClick={e => { e.preventDefault(); handleCartUpdate({ id: product, amount: 1, unit_amount: unit_amount, images: images, currency: currency}) }}>add to cart</button>
-                        </form>
-                    </section>
-                    )
-                }
-                return (
-                    <>
-                    <section className="main-shopitem-container">
-                    <div className='main-shopitem-text-container'>
-                        <div className="image">
-                            <h1>{name}</h1>
-                            <img alt={images} src={images} className='product-image' />
-                        </div>
-                        <div className='description-container'>
-                            <p>{description}</p>
-                        </div>
-                    </div>
-                    <form className='main-logic-container'>
-                        <p className="sr-legal-text">{`$${unit_amount}`}</p>
-                        <button onClick={e => { e.preventDefault(); handleCartUpdate({ id: product, amount: 1, unit_amount: unit_amount, images: images, name: name, currency:currency}) }}>add to cart</button>
-                    </form>
-                    </section>
-                    </>
-                )
-            })}
+    <>
+            {!loading &&
+                <div className="sr-main">
+                    {items.map(({ id, images, description, active, unit_amount, product, name, currency }) => {
+                        if (!active) {
+                            return (
+                                <section key={id} className="main-shopitem-container">
+                                    <h1> {name} </h1>
+                                    <div className='main-shopitem-text-container'>
+                                        <div className="image">
+                                            <img alt={images} src={images} className='product-image' />
+                                        </div>
+                                        <div className='description-container'>
+                                            <p>{description}</p>
+                                        </div>
+                                    </div>
+                                    <form className='main-logic-container'>
+                                        <p className="sr-legal-text" >{unit_amount}</p>
+                                        <button onClick={e => { e.preventDefault(); handleCartUpdate({ id: product, amount: 1, unit_amount: unit_amount, images: images, currency: currency }) }}>add to cart</button>
+                                    </form>
+                                </section>
+                            )
+                        }
+                        return (
+                            <>
+                                <section className="main-shopitem-container">
+                                <h1>{name}</h1>
+                                    <div className='main-shopitem-text-container'>
+                                        
+                                        <div className="image">
+                                            <img alt={images} src={images} className='product-image' />
+                                        </div>
+                                        <div className='description-container'>
+                                            <p>{description}</p>
+                                        </div>
+                                        <form className='main-logic-container'>
+                                        <p className="sr-legal-text">{`$${unit_amount/100}`}</p>
+                                        <button onClick={e => { e.preventDefault(); handleCartUpdate({ id: product, amount: 1, unit_amount: unit_amount, images: images, name: name, currency: currency }) }}>add to cart</button>
+                                    </form>
+                                    </div>
+
+                                </section>
+                            </>
+                        )
+                    })}
            
-        </div>
+                </div>
+            }
+            </>
     );
 }
 
