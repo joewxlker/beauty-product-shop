90c1694fed0424b53688608d56e0be95d3589378
joseph <joewxlk3r@gmail.com>
2022-07-22 16:23:18 +0800

    added honeypot

--- client/src/Components/FormComponents/Forms.jsx
+++ client/src/Components/FormComponents/Forms.jsx
@@ -10,7 +10,14 @@ const Forms = ({ formType, onFormChange, value, onTypeChange, error, origin }) =
   
   return (
     <>
-    <input className='createform-input' placeholder={formType} name={formType} value={value[formType]} onChange={handleChange} />
+      <input
+        name="honeypot"
+        style={{ opacity: '0%', cursor: 'default', height: '1px', width: '1px' }}
+        tabindex="-1"
+        autocomplete="off"
+        value={value['honeypot']}
+        onChange={handleChange} />
+       <input className='createform-input' placeholder={formType} name={formType} value={value[formType]} onChange={handleChange} />
       {error[formType] && <ErrorMessage formType={formType} />}
     </>
   )
