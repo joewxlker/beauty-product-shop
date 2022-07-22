2bfc6ea066c3d462e5d5f8fe17b0e1c261f5e070
joseph <joewxlk3r@gmail.com>
2022-07-22 16:29:07 +0800

    added routes to new components, added handleAccount logic and required props

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Canceled from './Stripe/Checkout/canceled';
 import Home from './Components/Home/Home'
 import Header from './Components/HeaderComponents/Header';
 import Footer from './Components/FooterComponents/Footer';
+import SuccessLogin from './UserAuth/Signup/SuccessLogin';
+import EmptyRedirect from './Stripe/Checkout/Empty';
 
 function App() {
 
@@ -23,14 +25,20 @@ function App() {
   const [cartItems, setCart] = useState([]);
   const [bool, setBool] = useSetBool();
   const [mobile, setIsMobile] = useState(false)
+  const [loggedStatus, setLoggedInStatus] = useState(false);
 
+  useEffect(() => {
+    getLocalData('userInfo').then((data) => {
+      if (data !== null) { setLoggedInStatus(true) }
+      console.log(loggedStatus)
+    })
+}, [loggedStatus, setLoggedInStatus])
 
   useEffect(() => {
       if (window.innerWidth < 600) setIsMobile(true)
       if (window.innerWidth > 600) setIsMobile(false)
     window.addEventListener('resize', resize);
     window.removeEventListener('resize', resize());
-    
   }, [mobile, setIsMobile]);
 
   const resize = () => {
@@ -105,16 +113,18 @@ function App() {
     <>
       
       <BrowserRouter>
-      <Header
-                bool={bool}
-                mobile={mobile}
-                data={data}
-                onRemoveItem={removeItem}
-                onToggle={setBool}
-                handleQuantityChange={handleQuantity}
-                cartItems={cartItems}
-                items={price}
-                updateCartItems={addItem}
+        <Header
+          setLoggedIn={(boolean) => { console.log(boolean); setLoggedInStatus(boolean)}}
+          bool={bool}
+          mobile={mobile}
+          data={data}
+          onRemoveItem={removeItem}
+          onToggle={setBool}
+          handleQuantityChange={handleQuantity}
+          cartItems={cartItems}
+          items={price}
+          updateCartItems={addItem}
+          loggedStatus={loggedStatus}
               />
         <Routes>
 
@@ -127,18 +137,25 @@ function App() {
               handleQuantityChange={handleQuantity}
               cartItems={cartItems}
               items={price}
-              updateCartItems={addItem} />
+              updateCartItems={addItem}
+              loggedStatus={loggedStatus}/>
           } />
           
           <Route path="/checkout" element={
             <Checkout
               onRemoveItem={removeItem}
-              handleQuantityChange={handleQuantity}/>
+              handleQuantityChange={handleQuantity}
+              loggedStatus={loggedStatus}/>
           } />
 
           <Route path="/createaccount" element={
             <CreateAccount/>
           } />
+
+          <Route path="/successlogin" element={<SuccessLogin
+            setLoggedIn={(boolean) => { console.log(boolean); setLoggedInStatus(boolean) }}
+          />} />
+          <Route path="/emptyredirect" element={<EmptyRedirect/>}/>
 
           <Route path='/products' element={
             <>
@@ -150,7 +167,8 @@ function App() {
                 handleQuantityChange={handleQuantity}
                 cartItems={cartItems}
                 items={price}
-                updateCartItems={addItem} />
+                updateCartItems={addItem}
+                loggedStatus={loggedStatus}/>
             </>
           } />
 
