794877568f0407b427a08c190ab9b7a58b2392dd
joseph <joewxlk3r@gmail.com>
2022-07-23 21:45:11 +0800

    added hash/salt comparison function

--- backend/src/Bcrypt/passwordAuth.ts
+++ backend/src/Bcrypt/passwordAuth.ts
@@ -2,10 +2,11 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 export const createHash = async (password: string) => {
-    console.log(`creating hash key from ${password}`)
     const hashed = await bcrypt.hash(password, saltRounds)
     return hashed
 }
 
-//TODO
-//export const queryHash
+export const compare = async (password: string, hash: string) => {
+    console.log(password, hash)
+    return await bcrypt.compare(password, hash)
+}
